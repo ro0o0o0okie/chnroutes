f160678dd11819865eed158614cbecdaf764066f
RayN <nemo.ss433@gmail.com>
2015-01-29 11:48:58 +0800

    remove unnecessary chnroutesapp files.

--- chnroutesapp/models.py
+++
@@ -1,7 +0,0 @@
-from google.appengine.ext import db
-
-
-class MyFile(db.Model):
-    name = db.StringProperty(required=True)
-    blob_key = db.StringProperty(required=True)
-    update_date = db.DateProperty()
